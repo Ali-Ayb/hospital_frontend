6af5a59fde6710081aef2849742dcbde77307a53
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-13 01:18:54 +0200

    added get rooms where room number appear after departmnet is choosen

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ window.onload = () => {
   base_url = "http://localhost/hospital_backend/";
 
   let department_dropdown = document.getElementById("department_dropdown");
+  let rooms_dropdown = document.getElementById("rooms_dropdown");
   let choose_department_btn = document.getElementById("choose_department_btn");
+  let choose_room_btn = document.getElementById("choose_room_btn");
 
   const token = localStorage.getItem("jwt");
   const config = {
@@ -21,16 +23,32 @@ window.onload = () => {
     });
   };
 
+  const getRooms = ($department) => {
+    let data = "";
+
+    let info = new FormData();
+    info.append("department", $department);
+
+    axios.post(`${base_url}get_rooms.php`, info, config).then((result) => {
+      result.data.forEach((element) => {
+        data += `
+          <option value="${element.room_number}">${element.room_number}</option>
+                `;
+      });
+      rooms_dropdown.innerHTML = data;
+    });
+  };
+
   const addUserToDepartment = () => {
     let department = department_dropdown.value;
 
     let data = new FormData();
     data.append("department", department);
+    getRooms(department);
 
     axios
       .post(`${base_url}choose_department.php`, data, config)
       .then((result) => {
-        console.log(result.data.response);
         if (result.data.response) {
           alert("department choosen successfuly");
         }
