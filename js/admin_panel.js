31a6c348867a37e3ea085b2a12e61ed1aefaf64f
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-12 14:52:52 +0200

    added assign_btn

--- js/admin_panel.js
+++ js/admin_panel.js
@@ -1,2 +1,3 @@
 const patients_dropdown = document.getElementById("patients_dropdown");
 const hospitals_dropdown = document.getElementById("hospitals_dropdown");
+const assign_btn = document.getElementById("assign_btn");
