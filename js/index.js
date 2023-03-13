c87cae2cbf1d629bdcd4ea054d347960d25bd1c5
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-13 02:00:44 +0200

    added medication

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ window.onload = () => {
 
   let department_dropdown = document.getElementById("department_dropdown");
   let rooms_dropdown = document.getElementById("rooms_dropdown");
+  let medications_dropdown = document.getElementById("medications_dropdown");
   let choose_department_btn = document.getElementById("choose_department_btn");
   let choose_room_btn = document.getElementById("choose_room_btn");
+  let choose_medication_btn = document.getElementById("choose_medication_btn");
 
   const token = localStorage.getItem("jwt");
   const config = {
@@ -39,6 +41,18 @@ window.onload = () => {
     });
   };
 
+  const getMedications = () => {
+    let data = "";
+    axios.get(`${base_url}get_medications.php`, config).then((result) => {
+      result.data.forEach((element) => {
+        data += `
+          <option value="${element.name}">${element.name}</option>
+                `;
+      });
+      medications_dropdown.innerHTML = data;
+    });
+  };
+
   const addUserToDepartment = () => {
     let department = department_dropdown.value;
 
@@ -55,6 +69,22 @@ window.onload = () => {
       });
   };
 
+  const assignMedication = () => {
+    let medication = medications_dropdown.value;
+
+    let data = new FormData();
+    data.append("medication", medication);
+    data.append("quantity", quantity);
+
+    axios.post(`${base_url}add_medication.php`, data, config).then((result) => {
+      if (result.data.response) {
+        alert("medication added successfuly");
+      }
+    });
+  };
+
   getDepartments();
+  getMedications();
   choose_department_btn.addEventListener("click", addUserToDepartment);
+  choose_medication_btn.addEventListener("click", assignMedication);
 };
